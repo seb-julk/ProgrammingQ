6a7d99384f3320e2d6076421386a73c521f94b5b
seb-julk <136075209+seb-julk@users.noreply.github.com>
2023-06-09 16:40:39 +0200

    Create cpp_example.cpp

---
+++ Newton Raphson Uni/cpp_example.cpp
@@ -0,0 +1,48 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+
+using namespace Rcpp;
+using namespace arma;
+
+double CallR(Function func, double x)
+{
+    // allows passing R function to cpp double
+    SEXP func_val = func(x);
+    return *REAL(func_val);
+}
+
+// [[Rcpp::export]]
+List NewtonRaphson(Function func, Function func_prime, double x_0, double tol, int maxiter)
+{
+    // initialize variables
+    List newtonraphson;
+    int i = 0;
+
+    while ((abs(CallR(func, x_0)) > tol) && (i < maxiter))
+    {
+        x_0 = x_0 - (CallR(func, x_0) / CallR(func_prime, x_0));
+        i += 1;
+
+        // comment out to not print
+        Rcout << i << ": " << x_0 << "\n";
+    }
+
+    if (abs(CallR(func, x_0)) < tol)
+    {
+        newtonraphson["root"] = x_0;
+        newtonraphson["iterations"] = i;
+        newtonraphson["value at root"] = CallR(func, x_0);
+        newtonraphson["convergence"] = "Achieved";
+
+        return newtonraphson;
+    }
+    else
+    {
+        newtonraphson["root"] = NA_REAL;
+        newtonraphson["iterations"] = i;
+        newtonraphson["value at root"] = NA_REAL;
+        newtonraphson["convergence"] = "Not achieved";
+
+        return newtonraphson;
+    }
+}
