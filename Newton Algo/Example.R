b25aed184af3e7d3c7f53eb7b91c76f128b5e47d
seb-julk <136075209+seb-julk@users.noreply.github.com>
2023-06-09 17:06:14 +0200

    Update Example.R

--- Newton Algo/Example.R
+++ Newton Algo/Example.R
@@ -1,23 +1,51 @@
 ## Algo:
 # Remember that we do not need f here, but can be necessary if function value at optimum is needed.
 
-NM <- function(f, f_prime, f_sec, dX0, dTol = 1e-9, n.max = 1000){
-  dX <- dX0
-  fx <- f(dX) # can be deleted
-  fpx <- f_prime(dX)
-  fsx <- f_sec(dX)
-  n <- 0
-  while ((abs(fpx) > dTol) && (n < n.max)) {
-    dX <- dX - fpx/fsx
-    fx <- f(dX) # can be deleted
-    fpx <- f_prime(dX)
-    fsx <- f_sec(dX)
-    n <- n + 1
-    cat("At iteration", n, "the value of x is:", dX, "\n")
-  }
-  if (n == n.max) {
-    cat('newton failed to converge\n')
-  } else {
-    return(dX)
-  }
+newton <- function(
+                   func,
+                   func_prime,
+                   func_sec,
+                   x_0,
+                   tol = 1e-9,
+                   maxiter = 1000) {
+    # initialize variables
+    i <- 0
+
+    while ((abs(func_prime(x_0)) > tol) && (i < maxiter)) {
+        x_0 <- x_0 - (func_prime(x_0) / func_sec(x_0))
+        i <- i + 1
+        cat(i, ": x =", x_0, "\n")
+    }
+
+    if (i == maxiter) {
+        return(
+            list(
+                optimum = NULL,
+                iterations = i,
+                f.optimum = NULL,
+                f_prime.optimum = NULL,
+                f_sec.optimum = NULL,
+                type = NULL,
+                convergence = "Not achieved"
+            )
+        )
+    } else {
+        return(
+            list(
+                optimum = x_0,
+                iterations = i,
+                f.optimum = func(x_0),
+                f_prime.optimum = func_prime(x_0),
+                f_sec.optimum = func_sec(x_0),
+                type = ifelse(
+                    func_sec(x_0) < 0,
+                    "Local maximum",
+                    "Local minimum"
+                ),
+                convergence = "Achieved"
+            )
+        )
+    }
+
+    return(x_0)
 }
