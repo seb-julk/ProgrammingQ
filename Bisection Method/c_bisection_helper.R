8c587f4effe17c9c22f2045e05c3a3b76fb06495
seb-julk <136075209+seb-julk@users.noreply.github.com>
2023-06-09 16:46:11 +0200

    Add files via upload

---
+++ Bisection Method/c_bisection_helper.R
@@ -0,0 +1,14 @@
+library(Rcpp)
+library(RcppArmadillo)
+
+objective <- function(x, m = 15, l = 0.1) {
+    return(l - ((x * (x + 1)**m) / ((x + 1)**m + 1)))
+}
+
+sourceCpp("c_bisection.cpp")
+
+BiSection(func = objective, x_l = -10, x_r = 10, tol = 1e-5)
+
+
+# verify solution
+uniroot(objective, interval = c(-100, 100))
