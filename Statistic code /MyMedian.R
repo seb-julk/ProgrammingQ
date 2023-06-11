8aa7e0c8632be7f290952c091048c9a1b1e72922
seb-julk <136075209+seb-julk@users.noreply.github.com>
2023-06-11 16:47:20 +0200

    Update and rename Absolut valeu.R to MyMedian.R

--- Statistic code /MyMedian.R
+++ Statistic code /MyMedian.R
@@ -1,5 +1,5 @@
-# Following gives the absolute value without using R commando
-absolute <- function(mX){
+# Following gives the median for each column in mX without using R commando
+MyMedian <- function(mX){
   for (i in 1:iN){
         vSort <- sort(mX[,i])
         vStorage[i] <- ifelse(iT%%2==1,mSort[(iT+1)/2],mean(vSort[iT/2+0:1]))
