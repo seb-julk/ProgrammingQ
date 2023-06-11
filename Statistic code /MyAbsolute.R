0ce17c39b7d2a217a1ae92855847fc58358b97bd
seb-julk <136075209+seb-julk@users.noreply.github.com>
2023-06-11 16:49:56 +0200

    Update MyAbsolute.R

--- Statistic code /MyAbsolute.R
+++ Statistic code /MyAbsolute.R
@@ -4,11 +4,11 @@ myAbs <- function(iT, iN, mX){
 mAbs <- matrix(NA, nrow = iT, ncol = iN)
     for (i in 1:iN){
         for (t in 1:iT){
-            if ((mX[t,i]- median[i]) >= 0){
-                mAbs[t,i] <- mX[t,i]- median[i] 
+            if (mX[t,i] >= 0){
+                mAbs[t,i] <- mX[t,i] 
             }
             else{
-                mAbs[t,i] <- -1 * (mX[t,i]-median[i])
+                mAbs[t,i] <- -1 * mX[t,i]
             }
         }
     }
