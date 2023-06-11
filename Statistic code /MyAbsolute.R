24405c795d2a73b4de1ecc04891b6b6c4e98e33c
seb-julk <136075209+seb-julk@users.noreply.github.com>
2023-06-11 16:46:34 +0200

    Create MyAbsolute.R

---
+++ Statistic code /MyAbsolute.R
@@ -0,0 +1,16 @@
+Find the absolute values for each column in a matrix
+
+myAbs <- function(iT, iN, mX){
+mAbs <- matrix(NA, nrow = iT, ncol = iN)
+    for (i in 1:iN){
+        for (t in 1:iT){
+            if ((mX[t,i]- median[i]) >= 0){
+                mAbs[t,i] <- mX[t,i]- median[i] 
+            }
+            else{
+                mAbs[t,i] <- -1 * (mX[t,i]-median[i])
+            }
+        }
+    }
+    return(mAbs)
+}
